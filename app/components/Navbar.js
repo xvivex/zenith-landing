9696eb81eb6fb4da0a80b034adef88df76228809
onkarrohit1220-ux <onkar.rohit1220@gmail.com>
2025-09-15 22:28:30 +0530

    Update Navbar.js

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -18,7 +18,7 @@ export default function Navbar() {
         </div>
 
         <div className="flex items-center">
-          <button className="flex items-center space-x-2 px-6 py-2 border border-gray-300 rounded-full text-gray-800 text-sm font-medium hover:bg-gray-200 transition-colors duration-200">
+          <button className="flex items-center space-x-2 px-6 py-2 cursor-pointer border border-gray-300 rounded-full text-gray-800 text-sm font-medium hover:bg-black hover:text-amber-50 transition-colors duration-200">
             <span>Contact Us</span>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14 5l7 7m0 0l-7 7m7-7H3" />
